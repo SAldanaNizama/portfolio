11424fff6afa773483f501bbdf7a731e2427f58b
SAldanaNizama <140188751+SAldanaNizama@users.noreply.github.com>
2024-06-09 23:31:42 -0500

    update-portfolio-responsive

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 function scrollToElement(elementSelector, instance = 0) {
+      const elements = document.querySelectorAll(elementSelector);
 
-  const elements = document.querySelectorAll(elementSelector);
+      if (elements.length > instance) {
+        elements[instance].scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+console.log(scrollToElement);
+    const link1 = document.getElementById("link1");
+    const link2 = document.getElementById("link2");
+    const link3 = document.getElementById("link3");
 
-  if (elements.length > instance) {
-    
-    elements[instance].scrollIntoView({ behavior: 'smooth' });
-  
-  }
-}
-
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-
-link1.addEventListener('click', () => {
+    link1.addEventListener('click', (event) => {
+  event.preventDefault();
   scrollToElement('.header');
-})
-link2.addEventListener('click', () => {
+});
+link2.addEventListener('click', (event) => {
+  event.preventDefault();
   scrollToElement('.header', 1);
-})
-link3.addEventListener('click', () => {
+});
+link3.addEventListener('click', (event) => {
+  event.preventDefault();
   scrollToElement('.column');
-})
+});
